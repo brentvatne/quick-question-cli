74f9350244ff8feb6b7cd7610bc0335cd73deb01
Brent Vatne <brentvatne@gmail.com>
2023-03-24 16:12:43 -0700

    Summarize URL

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@
 
 import { Command } from "commander";
 import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
+import cheerio from "cheerio";
 import * as dotenv from "dotenv";
 import os from "os";
 
@@ -27,24 +29,28 @@ const openai = new OpenAIApi(configuration);
 const program = new Command();
 
 // Import the package.json file
-const packageJson = require('./package.json');
+const packageJson = require("./package.json");
 
 program
   .version(packageJson.version)
   .description(
     "A CLI tool for asking questions to ChatGPT using the OpenAI node.js library"
   )
-  .argument("<question>", "The question to ask ChatGPT")
+  .argument("[question]", "The question to ask ChatGPT")
+  .option("-u, --url <url>", "The URL of the page to summarize")
   .helpOption("-h, --help", "Display help for command")
   .parse(process.argv);
 
+const options = program.opts();
 const question = program.args[0];
 
-if (question) {
+if (options.url) {
+  summarizeURL(options.url);
+} else if (question) {
   askChatGPT(question);
 } else {
   console.error(
-    "Error: No question provided. Use --help flag for more information."
+    "Error: No question or URL provided. Use --help flag for more information."
   );
   process.exit(1);
 }
@@ -57,6 +63,19 @@ async function askChatGPT(question: string) {
     });
 
     console.log(response.data.choices[0]?.message?.content);
+  } catch (error: any) {
+    console.error("Error:", error);
+  }
+}
+
+async function summarizeURL(url: string) {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const pageTitle = $("title").text();
+    const summaryQuestion = `Please summarize the content of the web page at ${url} with title "${pageTitle}"`;
+
+    askChatGPT(summaryQuestion);
   } catch (error: any) {
     console.error("Error:", error);
   }
